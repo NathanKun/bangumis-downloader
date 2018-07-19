e6ba4b4afdb9b94457604030bb9fd870df439cab
HE Junyang <nathanhejunyang@gmail.com>
2018-07-19 15:07:37 +0200

    Update logger, can write log to file.

--- logger.py
+++ logger.py
@@ -4,9 +4,19 @@ Created on 18 juil. 2018
 @author: NathanKun
 '''
 
-import datetime
+import os, datetime
 
-def log(logStr: str):
-    timeStr = '{0:%Y-%m-%d_%H:%M:%S.%f}'.format(datetime.datetime.now())[:-3] + ": "
+logFilePath = "log/bmu_{0}.log".format("{0:%Y%m%d_%H%M%S}".format(datetime.datetime.now()))
+if not os.path.exists("log"):
+    os.mkdir("log")
+
+
+def log(logStr: str, withTime=True):
+    timeStr = ''
+    if withTime:
+        timeStr = '{0:%Y-%m-%d_%H:%M:%S.%f}'.format(datetime.datetime.now())[:-3] + ": "
     logStr = timeStr + logStr
     print(logStr)
+    
+    with open(logFilePath, "a", encoding='utf8') as f:
+        f.write(logStr + "\n")
